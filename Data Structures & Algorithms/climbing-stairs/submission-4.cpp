6a99af4251b4f633aae02a092613ea2911c59279
Avneesh Kumar <54072374+Cybertron-Avneesh@users.noreply.github.com>
2026-09-13 12:41:41 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        int i_2 = 1;
+        int i_1 = 2;
+        int curr = 0;
+        for(int i=3; i<=n; i++){
+            curr = i_1 + i_2;
+            i_2 = i_1;
+            i_1 = curr;
+        }    
+        return curr;
+    }
+};
